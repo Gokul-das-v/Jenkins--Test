66d0b93a6d666508bbc44946682e3fed0897b513
Gokul Das V <126198458+Gokul-das-v@users.noreply.github.com>
2023-04-15 11:55:29 +0530

    update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 app = Flask(__name__)
 
 CHANNELS = {
-  'qazi': 'UCqrILQNl5Ed9Dz6CGMyvMTQ',
+  'qazi': 'UCqrILQNl5Ed9Dz6CGMyvMTQ', 
   'mrbeast': 'UCX6OQ3DkcsbYNE6H8uQQuVA',
   'mkbhd': 'UCBJycsmduvYEL83R_U4JriQ',
   'pm': 'UC3DkFux8Iv-aYnTRWzwaiBA',
